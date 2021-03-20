3bcdba61df0e11e45326bd20be17737e8f458336
Sachin-Sendhav <sacchu1714@gmail.com>
2021-03-20 16:39:20 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './Comopnents/ClassCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <ClassCounter />
+    </div>
+  );
+}
+
+export default App;
